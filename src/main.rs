5b2445d541738ed714b27ac02c0ce9824947f3d9
hazzarust <itshazzag@gmail.com>
2024-12-08 21:38:13 +0000

    Telegram bot that gets the price of HYPE from coinmarketcap API via teloxide

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,38 @@
-use teloxide::prelude::*;
+mod coinmarketcap;
+mod telegram_bot;
+
 use dotenv::dotenv;
 use std::env;
-
-mod coinmarketcap;
+use crate::coinmarketcap::hype_price;
+use crate::telegram_bot::{Command, answer};
+use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() {
 
     // Loads env variables
-
     dotenv().ok();
-
+    
     // Check that the RUST_LOG environment variable is set correctly
 
     if let Ok(log_level) = env::var("RUST_LOG") {
         println!("Logging level: {}", log_level);
     }
 
-    // Optional: Print the loaded token to verify it's being read correctly
-    
-    if let Ok(teloxide_token) = env::var("TELOXIDE_TOKEN") {
-        println!("Loaded TELOXIDE_TOKEN: {}", teloxide_token);
-    } else {
-        println!("TELOXIDE_TOKEN is not set!");
-    }
-
-    if let Ok(cmk_token) = env::var("COINMARKETCAP_TOKEN"){
-        println!("Loaded COINMARKETCAP_TOKEN token {}", cmk_token)
-    }else{
-        println!("COINMARKETCAP_TOKEN is not set!")
-    }
-
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting command bot...");
 
     let bot = Bot::from_env();
 
-    //takes in my bot from .env, 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
-
-
-
+    Command::repl(bot, answer).await;
 
 
 }
+
+
+
+
+
+
+
+
